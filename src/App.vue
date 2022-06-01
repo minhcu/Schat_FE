09a45e2d783c36f4ff93f0b3688bf71aaf8c6575
minhcu <minhdong1831999@gmail.com>
2022-06-01 13:56:45 +0700

    NEW BASE/fixed layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <component
+    :is="layout"
+    :class="{
+      dark: isDarkMode,
+    }"
+  >
+    <router-view @layout="updateLayout" />
+  </component>
 </template>
+<script>
+// Import layouts here
+import DefaultLayout from "./layouts/defaultLayout.vue";
+import Auth from "./layouts/authLayout.vue";
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+export default {
+  name: "App",
+  components: {
+    DefaultLayout,
+    Auth,
+  },
+  data() {
+    return {
+      layout: "defaultLayout",
+      isDarkMode: false,
+    };
+  },
+  methods: {
+    updateLayout(layout) {
+      this.layout = layout;
+    },
+    toggleDarkMode() {
+      this.isDarkMode = this.isDarkMode ? false : true;
+    },
+  },
+};
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
